ab52ffadce7be43af413ca6f00414a921600c699
Iliana Markova <iliana.markova@estafet.com>
2022-05-13 13:35:57 +0300

    minor update

---
+++ src/test/java/helpers/CalculatorHelper.java
@@ -0,0 +1,12 @@
+package helpers;
+
+public class CalculatorHelper {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
